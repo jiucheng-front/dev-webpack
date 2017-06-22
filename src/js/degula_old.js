1fb4f24665d758f91577e3f8644e689dc2bd49d4
wangjianfei <444128852@qq.com>
2017-06-22 12:12:24 +0800

    測試degulaOne的GET請求

--- src/js/degula_old.js
+++ src/js/degula_old.js
@@ -1,222 +1,74 @@
 !function(){
-  var isNotAjax=true;
-  // 假数据
-  if(isNotAjax){
-  	var data={
-  	  "ret_code": "0",
-  	  "ret_msg": "ok",
-  	  "data": {
-  	    "my": {
-  	      "live_id": "10008401493805331",
-  	      "live_key": "0MYaTv",
-  	      "stream_direction": 1,
-  	      "live_url": "rtmp://video.obslanglive.com/live/10008401493805331",
-  	      "headimg": "http://blob.ufile.ucloud.com.cn/f59a66b97b2cbd9c3e469b5c0bf4e3015f101af8bd24e2328a4446076b226440",
-  	      "nickname": "(。•́︿•̀。)108",
-  	      "pfid": 1000840,
-  	      "follow_status": 0
-  	    },
-  	    "users": [
-  	      {
-  	        "pfid": 1000011,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 19,
-  	        "promotion": 1,
-  	        "live_id": null,
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://wx.qlogo.cn/mmopen/ajNVdqHZLLASXYCVQQeiaXKA07Mx1Lu24juBVZ40ib9IsI6ImibfsWD1R3JS5I5L4LZMmgxpC4B7Y1taCZmXHQVKic4lbHibcntI7L6S7K4gnMW8/0",
-  	        "nickname": "cabbageancy"
-  	      },
-  	      {
-  	        "pfid": 1000004,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 17,
-  	        "promotion": 1,
-  	        "live_id": "13154646",
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://playback-langlive.ufile.ucloud.cn/4825891161c4d9a377be0785078c2158.jpg",
-  	        "nickname": "减肥沟沟壑壑健健康康快快"
-  	      },
-  	      {
-  	        "pfid": 1023841,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 15,
-  	        "promotion": 1,
-  	        "live_id": "65465465465",
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://blob.ufile.ucloud.com.cn/9997f3e49dbf7d65cc495f1a8e21d33cfcac35ede39d4830580a66085ea45832",
-  	        "nickname": "优雅的猫"
-  	      },
-  	      {
-  	        "pfid": 1000044,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 13,
-  	        "promotion": 1,
-  	        "live_id": null,
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://blob.ufile.ucloud.com.cn/92cee6a8ce12f7b01a58fd7fa990a1bb.jpg",
-  	        "nickname": "嘻嘻哈哈😄💔1213"
-  	      },
-  	      {
-  	        "pfid": 1000048,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 10,
-  	        "promotion": 1,
-  	        "live_id": null,
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://blob.ufile.ucloud.com.cn/c8d240b8dfb870238431f43a392b312fba00a22c15230aa66f8e33cacc6debda",
-  	        "nickname": "珍珠奶茶😱芋頭紅豆薏米"
-  	      },
-  	      {
-  	        "pfid": 1000830,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 9,
-  	        "promotion": -1,
-  	        "live_id": null,
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://blob.ufile.ucloud.com.cn/a765a85a32fde0343476d3d5a54ce686d9370cd4e35f4ac941661c2ab6746bcd",
-  	        "nickname": "58665"
-  	      },
-  	      {
-  	        "pfid": 1000941,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 8,
-  	        "promotion": 0,
-  	        "live_id": null,
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://playback-langlive.ufile.ucloud.cn/41395b6c4851bbc8d2ac49b643ea1420",
-  	        "nickname": "随意"
-  	      },
-  	      {
-  	        "pfid": 1000052,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 7,
-  	        "promotion": -1,
-  	        "live_id": null,
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://blob.ufile.ucloud.com.cn/9256deb2c705849daef63ace024d4fe121e68f712eefdc585692247a52cf1920",
-  	        "nickname": "upper"
-  	      },
-  	      {
-  	        "pfid": 1000733,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 6,
-  	        "promotion": 0,
-  	        "live_id": null,
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://playback-langlive.ufile.ucloud.cn/8422f67af167df7d2b2caa78ac0a4773c4eade4575b343b5a82b7a48c4bf1427",
-  	        "nickname": "查房"
-  	      },
-  	      {
-  	        "pfid": 1000012,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 5,
-  	        "promotion": -1,
-  	        "live_id": null,
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://blob.ufile.ucloud.com.cn/0d94ae9dc33721c87af78a7b2099f5a7ab7395972444c7ab979e3dea46e8ba2e",
-  	        "nickname": "c224002"
-  	      },
-  	      {
-  	        "pfid": 1000039,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 4,
-  	        "promotion": 0,
-  	        "live_id": "10000391494820650",
-  	        "live_key": "UioEgF",
-  	        "stream_direction": 0,
-  	        "live_url": "rtmp://video.langlive.com/live/10000391494820650",
-  	        "headimg": "http://blob.ufile.ucloud.com.cn/25f0e2243ac9f515ed3be2fb1854c8b7db8e04b195a088962179cf5937ae25dd",
-  	        "nickname": "哇呀唔唔無武呃呃😂哇咔"
-  	      },
-  	      {
-  	        "pfid": 1000049,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 3,
-  	        "promotion": -1,
-  	        "live_id": null,
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://blob.ufile.ucloud.com.cn/6b7f50b02ed10b72159ccf1f00a73ab9.jpg",
-  	        "nickname": "思考解决"
-  	      },
-  	      {
-  	        "pfid": 1000040,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 2,
-  	        "promotion": 0,
-  	        "live_id": null,
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://blob.ufile.ucloud.com.cn/3f8f9f008d9d34a9b4cdffd32a06a3d8.jpg",
-  	        "nickname": "🐾\"sting\"💤"
-  	      },
-  	      {
-  	        "pfid": 1000010,
-  	        "count": 0,
-  	        "follow_status": null,
-  	        "score": 1,
-  	        "promotion": -1,
-  	        "live_id": null,
-  	        "live_key": "",
-  	        "stream_direction": 0,
-  	        "live_url": "",
-  	        "headimg": "http://blob.ufile.ucloud.com.cn/f9cb7d8ac961fb9b3d5a882aea55bfe5",
-  	        "nickname": "gggggg"
-  	      }
-  	    ]
-  	  }
-  	  // 結尾
-  	}
-  }
+    // 1、原生的javascript
+    function nativeAjax(option,success,error){
+    	// 定义domain,方便环境切换,本地有防火墻阻止https請求
+      // var domain='https://' + window.location.host + '/';
+    	var domain='http://' + window.location.host + '/';
+    	var url=domain+option.urlStr;
+    	var type=option.ajaxType;
+    	var data=option.ajaxData;
+    	var xhrRequest=null;
+    	if(window.XMLHttpRequest){
+            xhrRequest = new XMLHttpRequest();
+        } else {
+            xhrRequest = new ActiveXObject('Microsoft.XMLHTTP')
+        }
+    	var str=null;
+    	xhrRequest.open(type,url,true);
+    	if(type==="POST"&&data!=null){
+    		xhrRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+    		for(var key in data){
+    			str+='&'+key+'='+data[key];
+    			str=str.slice(1);
+    		}
+    	}
+    	xhrRequest.onreadystatechange=function(){
+    		if(xhrRequest.readyState==4){
+    			if(xhrRequest.status==200){
+    				// 1.1、格式化返回的数据
+    				var responseData=JSON.parse(xhrRequest.responseText);
+    				// 1.2、这里操作数据--------
+    				success(responseData);
+    			}else{
+    				// 1.3、没成功返回HTTP状态码
+    				error(xhrRequest.status);
+    			}
+    		}
+    	}
+    	xhrRequest.send(str);
+    }
   //2.0 请求数据
   function getDate(){
   	// $.post(domain+'v2/activity/dracula_data', {"HTTP_USER_TOKEN":token, "HTTP_USER_UID":pfid, "anchor_pfid":anchor_id },
   	 	// function(data) {
               /*optional stuff to do after success */
-              if(data.ret_code=="0"){
-                  var users=data.data.users;
-                  sortData(users);
-              }
+              // if(data.ret_code=="0"){
+              //     var users=data.data.users;
+              //     sortData(users);
+              // }
   	    // },
   		// "json"
       // );
+      //4、GET：定义请求参数
+    var getOption={
+    	ajaxType:"GET",
+    	urlStr:"json-datas/degula/degula.json",
+    	ajaxData:null
+    }
+    nativeAjax(getOption,function(data){
+    	// 成功函数
+    	console.log(data);
+      if(data.ret_code=="0"){
+          var users=data.data.users;
+          sortData(users);
+      }
+    },function(error){
+    	// 失败返回HTTP状态码
+    	console.log(error);
+
+    });
   }
+  // 調用請求
   getDate();
   // 1 把人員按照積分从高到底排列
   function sortData(lists){

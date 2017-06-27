15660a019534f1f069ace163d4af0fa4796c2572
wangjianfei <444128852@qq.com>
2017-06-27 19:52:16 +0800

    添加预言家栏目的背景图以及样式

--- src/js/dracula.js
+++ src/js/dracula.js
@@ -22,18 +22,58 @@ console.log("测试");
 
   // 切换事件
   var btns=document.getElementsByClassName("js-nav-btn");
-  var items=document.getElementsByClassName("js-item");
+  var items=document.getElementsByClassName("content-item");
   for(var i=0;i<btns.length;i++){
     btns[i].index=i;
     btns[i].onclick=function(){
       for (var j = 0; j < items.length; j++) {
-        items[j].className="js-item";
+        items[j].className="content-item";
         btns[j].className="js-nav-btn";
       }
       btns[this.index].className="js-nav-btn selected";
-      items[this.index].className="js-item show";
+      items[this.index].className="content-item show";
     }
   }
+
+
+  // 添加事件
+  function addEvent(){
+    // 用戶列表投票
+    var voteBtns=document.getElementsByClassName("vote-btn");
+    for(var i=0;i<voteBtns.length;i++){
+      voteBtns[i].addEventListener("click",function(){
+        var pfid=this.getAttribute("data-pfid");
+        // 需要重新請求數據
+        alert(pfid);
+      });
+    }
+
+  }
+  addEvent();
+
+
+
+  // 返回頂部通用的方法
+  function backTop(btnId) {
+      var btn = document.getElementById(btnId);
+      var d = document.documentElement;
+      var b = document.body;
+      window.onscroll = set;
+      btn.style.display = "none";
+      btn.onclick = function() {
+          btn.style.display = "none";
+          window.onscroll = null;
+          this.timer = setInterval(function() {
+              d.scrollTop -= Math.ceil((d.scrollTop + b.scrollTop) * 0.1);
+              b.scrollTop -= Math.ceil((d.scrollTop + b.scrollTop) * 0.1);
+              if ((d.scrollTop + b.scrollTop) == 0) clearInterval(btn.timer, window.onscroll = set);
+          }, 10);
+      };
+      function set() {
+          btn.style.display = (d.scrollTop + b.scrollTop > 100) ? 'block': "none"
+      }
+  };
+  backTop('backTop');
 
 
 }();

4f8c4ff425fe415d386af4d0d226eec7aa56a08c
jiucheng <444128852@qq.com>
2017-11-23 23:28:12 +0800

    add demo rank list about pug

--- src/entry/entry.js
+++ src/entry/entry.js
@@ -31,6 +31,12 @@ var container = document.getElementById("md")
 container.innerHTML = html;
 
 // PUG 渲染
+/**
+ * DOV
+ * https://github.com/pugjs/pug-loader
+ * https://pugjs.org/api/reference.html
+ * 
+ * * */
 var template = require("./demo.pug");
 var pugBox = document.getElementById("pug");
 
@@ -53,3 +59,22 @@ var str = template({data:arr});
 console.log(str);
 pugBox.innerHTML = str;
 
+//
+var rank = require("../pug/rank.pug");
+var rankBox = document.getElementById("rankBox");
+
+var rankLists=[
+    {
+        src:"http://a.img.shouyintv.cn/jBfK301-normal",
+        name:"你是风儿我傻傻",
+        diamond:123
+    },
+    {
+        src:"http://a.img.shouyintv.cn/QDjD301-normal",
+        name:"个覅感觉对方考虑",
+        diamond:456654
+    }
+];
+
+var rankStr = rank({ data:rankLists });
+rankBox.innerHTML = rankStr;

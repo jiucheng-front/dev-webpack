a3ce367f5fcab7f5e151b8260e67f54eeed0ea86
wangjianfei <444128852@qq.com>
2017-11-29 21:44:21 +0800

    test import css

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 module.exports = {
     plugins: {
-      'postcss-import': {},
-      'postcss-cssnext': {
+        // css 内可以使用 @import 其他CSS文件
+        'postcss-import': {},
+        // CSS内可以使用 css3+ 新语法
+        'postcss-cssnext': {
         browsers: ['last 2 versions', '> 5%']
-      }
+        },
+        // CSS 内自动追加浏览器前缀
+        'autoprefixer':{}
     }
   }
--- webpack.common.js
+++ webpack.common.js
@@ -37,11 +37,11 @@ module.exports = {
                 test: /\.css$/,
                 // 6.2 想抽离出来得
                 use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
+                    fallback: "style-loader",
                     // use: 'css-loader',
                     use:[
-                        { loader: 'css-loader', options: { importLoaders: 1 }},
-                        'postcss-loader'
+                        { loader: "css-loader", options: { importLoaders: 1 }},
+                        "postcss-loader"
                     ]
                 })
             },
@@ -74,6 +74,7 @@ module.exports = {
                     fallback:"style-loader",
                     use:[
                         "css-loader",
+                        "postcss-loader",
                         "stylus-loader"
                     ]
                 })
@@ -147,6 +148,8 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'App',
             filename: 'index.html',
+            favicon:'./images/favicon.ico',
+            // minify:true,
             template: CONFIG.dev.indexFile
         }),
     ]

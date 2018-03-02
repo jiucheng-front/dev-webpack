a2646142978f3e82bc0c3ad2177b47e14cc7b9cc
wangjianfei <444128852@qq.com>
2018-03-02 17:07:19 +0800

    '异步追加组件简单逻辑'

--- src/entry/test.js
+++ src/entry/test.js
@@ -2,6 +2,21 @@ function Testfn(){
     let testDom = document.getElementById("test");
     console.log("异步组件调用成功");
     console.log(testDom);
+    // 动态追加DOM
+    var element = document.createElement('div');
+    var button = document.createElement('button');
+    var br = document.createElement('br');
+    button.innerHTML = 'Click me and look at the console!';
+    element.innerHTML = ['Hello', 'webpack'].join('');
+    element.appendChild(br);
+    element.appendChild(button);
+    document.body.appendChild(element);
+    // 异步追加的逻辑
+    button.addEventListener("click", e =>{
+        let targetElem = e.target;
+        console.log(e)
+        console.log(targetElem)
+    })
 }
 // Testfn()
 module.exports = Testfn

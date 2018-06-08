fe1a08cdae3d214dd42e52c44595e9fdd41fbfa5
wangjianfei <444128852@qq.com>
2018-06-08 19:05:33 +0800

    mock data

--- src/entry/mock/Mock.js
+++ src/entry/mock/Mock.js
@@ -2,7 +2,9 @@ const Mock = require("mockjs")
 let data = Mock.mock({
     'list|1-10': [{
         'id|+1': 1,
-        'nickname|1-2':"Curry"
+        'nickname': '@first @last',
+        'score|50-100': 100,
+        'tel|13000000000-14000000000':13000000000
     }]
 })
 let MockData = JSON.stringify(data)
